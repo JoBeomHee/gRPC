b4b2f340e8f1a09a98bee15bb53662c85da7bc26
JoBeomhee <>
2022-04-22 20:51:16 +0900

    서버 측 인터셉터

---
+++ gRPC 서버 측 인터셉터 예제/gRPCInterceptor/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using gRPCInterceptor;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7063");
+var greeterClient = new Greeter.GreeterClient(channel);
+
+var reply = await greeterClient.SayHelloAsync(new HelloRequest { Name = "Beom" });
+
+Console.WriteLine(reply.Message);
