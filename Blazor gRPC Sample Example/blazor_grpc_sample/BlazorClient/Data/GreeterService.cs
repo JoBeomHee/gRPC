738ecfc7ad93e4b65772e6397877734771574e4b
JoBeomhee <>
2022-04-23 22:04:22 +0900

    Blazor gRPC Sample Example

---
+++ Blazor gRPC Sample Example/blazor_grpc_sample/BlazorClient/Data/GreeterService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+
+namespace BlazorClient.Data
+{
+    public class GreeterService
+    {
+        public async Task<string> SayHello(string name)
+        {
+            using var httpClientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+
+            using var httpClient = new HttpClient(httpClientHandler);
+            using var channel = GrpcChannel.ForAddress("https://localhost:7005", new GrpcChannelOptions { HttpClient = httpClient });
+
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                    new HelloRequest { Name = name });
+
+            return reply.Message;
+
+        }
+    }
+}
